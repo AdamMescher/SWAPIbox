69c4958874e53ea219f1c793461b4cb4dfbe7ac0
Rufus <rufusasterisk@gmail.com>
2017-10-10 14:43:06 -0600

    Configure test env, Add button tests

--- src/Components/Button/Button.test.js
+++ src/Components/Button/Button.test.js
@@ -1,3 +1,39 @@
 import React from 'react';
 import Button from './Button';
 import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import config from '../../config';
+
+describe(`Button`, () => {
+  let wrapper, button, buttonText;
+  const mockFunction = jest.fn();
+
+  beforeEach( () => {
+    wrapper = shallow(
+      <Button
+        buttonText='Button Text'
+        buttonClass='button-class'
+        buttonCallback={mockFunction} />
+    );
+    button = wrapper.find('button');
+    buttonText = button.text();
+  });
+
+  test(`Can be created`, () => {
+    expect(wrapper.exists()).toEqual(true);
+  })
+
+  test(`Receives properties`, () => {
+    expect(button.type()).toEqual('button');
+    expect(buttonText).toEqual('Button Text');
+  })
+
+  test(`receives a class`, () => {
+    expect((button).hasClass('button-class')).toEqual(true);
+  })
+
+  test(`clicking the button triggers the function`, () => {
+    button.simulate('click');
+    expect(mockFunction.mock.calls.length).toBe(1)
+  })
+});
