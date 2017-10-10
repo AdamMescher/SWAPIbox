0487c86b9ffbded35c8ad64f38c1f361920e9880
Rufus <rufusasterisk@gmail.com>
2017-10-10 17:37:12 -0600

    refactor take 1

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
+let grossFactorHomeworld;
+let grossFactorSpecies;
+let grossFactorPerson;
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/people/')
+    .then(raw => raw.json())
+    .then(parsedData => {
+      console.log(parsedData)
+      const unresolvedHomeworldPromises = parsedData.results.map( (person) => {
+        return fetch(person.homeworld)
+        .then(homeworldRawData => homeworldRawData.json())
+        // .then(dataOutput => {console.log(dataOutput)})
+      });
+      const unresolvedSpeciesPromises = parsedData.results.map( (person) => {
+        return fetch(person.species[0])
+        .then(speciesRawData => speciesRawData.json())
+      })
+
+      Promise.all(unresolvedHomeworldPromises)
+      .then(homeworldData => {
+        console.log(homeworldData);
+        grossFactorHomeworld = homeworldData;
+      })
+      Promise.all(unresolvedSpeciesPromises)
+      .then(speciesData => {
+        console.log(speciesData);
+        grossFactorSpecies = speciesData
+      })
+
+      grossFactorPerson = parsedData;
+
+    })
+
+    // .then(noReturn => {
+    //   console.log(
+    //     grossFactorPerson.results.map( (person, index) => (
+    //       Object.assign({}, {
+    //         name: person.name,
+    //         species: grossFactorSpecies[index].name,
+    //         homeworld: grossFactorHomeworld[index].name,
+    //         homeworldPop: grossFactorHomeworld[index].population
+    //       })
+    //       )
+    //     )
+    //   );
+    // })
+
+
+  }
+
+
+
   render() {
     return (
       <div className="App">
