24398aacd33f6562b4f14bf635dff1c09b852cdd
Adam Mescher <admescher@gmail.com>
2017-10-10 14:13:51 -0600

    Add CardContainer component logic to render all cards passed via arrayOfNounObjects

---
+++ src/Components/CardContainer/CardContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+
+const CardContainer = ( arrayOfNounObjects ) => {
+
+  const mappedCards = arrayOfNounObjects.map( noun => {
+    return (
+      <li>
+        <Card nounObject={noun} />
+      </li>
+    )
+  })
+
+  return (
+    <div className="card-container">
+      <h2 className="card-container-title">Type</h2>
+      <ul>
+        {mappedCards}
+      </ul>
+    </div>
+  )
+}
