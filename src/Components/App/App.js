39ed9f4f99fe332dd90e728015214d6137bda354
Rufus <rufusasterisk@gmail.com>
2017-10-11 10:18:25 -0600

    Add functioning API call

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,39 +1,78 @@
 import React, { Component } from 'react';
-let grossFactorHomeworld;
-let grossFactorSpecies;
-let grossFactorPerson;
+// let grossFactorHomeworld;
+// let grossFactorSpecies;
+// let grossFactorPerson;
 
 class App extends Component {
 
   componentDidMount() {
-    fetch('https://swapi.co/api/people/')
-    .then(raw => raw.json())
-    .then(parsedData => {
-      console.log(parsedData)
-      const unresolvedHomeworldPromises = parsedData.results.map( (person) => {
-        return fetch(person.homeworld)
-        .then(homeworldRawData => homeworldRawData.json())
-        // .then(dataOutput => {console.log(dataOutput)})
-      });
-      const unresolvedSpeciesPromises = parsedData.results.map( (person) => {
-        return fetch(person.species[0])
-        .then(speciesRawData => speciesRawData.json())
+    fetch('https://swapi.co/api/people')
+      .then(raw => raw.json())
+      .then(parsedData => {
+        //fetch homeworld data
+        // let tempObject = {}
+        const unresolvedPromises = parsedData.results.map( (person, index, array) => {
+          let tempObject = {name: person.name};
+          return fetch(person.homeworld)
+            .then(homeworldRawData => homeworldRawData.json())
+            .then(homeworldData => {
+              Object.assign(tempObject, {
+                homeworld: homeworldData.name,
+                homeworldPop: homeworldData.population
+              })
+            })
+            .then(totallynewstuff => {
+              return fetch(person.species[0])
+                .then(speciesRawData => speciesRawData.json())
+                .then(speciesData => {
+                  // console.log('species ', speciesData);
+                  Object.assign(tempObject, {species: speciesData.name})
+                  return tempObject;
+                  // console.log(thing);
+                })
+                // .then(donzo => {
+                //   console.log(donzo)
+                //   return donzo;
+                // })
+            })
+        })
+        // console.log(unresolvedPromises);
+        Promise.all(unresolvedPromises)
+          .then(promiseAllResults => {
+            console.log(promiseAllResults);
+          })
       })
+  }
 
-      Promise.all(unresolvedHomeworldPromises)
-      .then(homeworldData => {
-        console.log(homeworldData);
-        grossFactorHomeworld = homeworldData;
-      })
-      Promise.all(unresolvedSpeciesPromises)
-      .then(speciesData => {
-        console.log(speciesData);
-        grossFactorSpecies = speciesData
-      })
-
-      grossFactorPerson = parsedData;
-
-    })
+  // componentDidMount() {
+  //   fetch('https://swapi.co/api/people/')
+  //   .then(raw => raw.json())
+  //   .then(parsedData => {
+  //     console.log(parsedData)
+  //     const unresolvedHomeworldPromises = parsedData.results.map( (person) => {
+  //       return fetch(person.homeworld)
+  //       .then(homeworldRawData => homeworldRawData.json())
+  //       // .then(dataOutput => {console.log(dataOutput)})
+  //     });
+  //     const unresolvedSpeciesPromises = parsedData.results.map( (person) => {
+  //       return fetch(person.species[0])
+  //       .then(speciesRawData => speciesRawData.json())
+  //     })
+  //
+  //     Promise.all(unresolvedHomeworldPromises)
+  //     .then(homeworldData => {
+  //       console.log(homeworldData);
+  //       grossFactorHomeworld = homeworldData;
+  //     })
+  //     Promise.all(unresolvedSpeciesPromises)
+  //     .then(speciesData => {
+  //       console.log(speciesData);
+  //       grossFactorSpecies = speciesData
+  //     })
+  //
+  //     grossFactorPerson = parsedData;
+  //
+  //   })
 
     // .then(noReturn => {
     //   console.log(
@@ -50,7 +89,7 @@ class App extends Component {
     // })
 
 
-  }
+  // }
 
 
 
