250647820194fe30d6d3a34f9ecb611d487c910c
Adam Mescher <admescher@gmail.com>
2017-10-12 11:17:17 -0600

    Add css scrolling animation to aside text

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -4,11 +4,8 @@ import PropTypes from 'prop-types';
 const Aside = ({movieData}) => {
   const randomIndex = Math.floor(  Math.floor(Math.random() * movieData.length + 1) )
   return(
-    <aside className="aside">
-      <section className="intro">A long time ago, in a galaxy far,<br /> far away....</section>
-      <p className="aside-opening-crawl">{movieData[randomIndex].text}</p>
-      <h4 className="aside-movie-title">{movieData[randomIndex].title}</h4>
-      <p className="aside-movie-date">{movieData[randomIndex].date}</p>
+    <aside className="aside scroll-down">
+      <p className="aside-opening-crawl">{movieData[randomIndex].text} <br /> <strong>{movieData[randomIndex].title}</strong> <br /> {movieData[randomIndex].date}</p>
     </aside>
   )
 }
